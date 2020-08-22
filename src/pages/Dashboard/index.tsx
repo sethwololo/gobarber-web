be9d3abd2e480640bd84994ab558d020827fc087
sethwololo <sethwololo@gmail.com>
2020-08-22 20:04:53 -0300

    Criando Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img src={user.avatar_url} alt="Matheus Henrique" />
+            <div>
+              <span>Bem vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
   );
 };
 
