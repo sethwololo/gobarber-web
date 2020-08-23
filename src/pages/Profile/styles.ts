1210b41269437cc2b0b250deb4b1ac0efed55550
sethwololo <sethwololo@gmail.com>
2020-08-23 05:44:23 -0300

    Troca de avatar

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -69,7 +69,7 @@ export const AvatarInput = styled.div`
     border-radius: 50%;
   }
 
-  button {
+  label {
     position: absolute;
     width: 48px;
     height: 48px;
@@ -79,10 +79,14 @@ export const AvatarInput = styled.div`
     right: 0;
     bottom: 0;
     transition: background-color 0.2s;
-
+    cursor: pointer;
     display: flex;
     place-items: center;
     place-content: center;
+
+    input {
+      display: none;
+    }
 
     svg {
       width: 20px;
