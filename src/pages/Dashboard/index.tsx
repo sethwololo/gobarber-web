3b98ea11cf75580a3640e2903c7b5167e6f58505
sethwololo <sethwololo@gmail.com>
2020-08-22 21:23:25 -0300

    Listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiPower, FiClock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
@@ -13,10 +13,14 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -61,6 +65,58 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/54484095?s=460&u=4f49e77eb6c6582ea2b2792b671cd4dcc18a7baa&v=4"
+                  alt="Matheus Henrique"
+                />
+                <strong>Matheus Henrique</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/54484095?s=460&u=4f49e77eb6c6582ea2b2792b671cd4dcc18a7baa&v=4"
+                  alt="Matheus Henrique"
+                />
+                <strong>Matheus Henrique</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                15:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/54484095?s=460&u=4f49e77eb6c6582ea2b2792b671cd4dcc18a7baa&v=4"
+                  alt="Matheus Henrique"
+                />
+                <strong>Matheus Henrique</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
